91f75a6f1b1820076b0e55a763570467e7487708
kuangzhenxi <kuangzhenxi@outlook.com>
2020-09-22 20:47:52 +0800

    v1

---
+++ luban_spring_v1/src/main/java/com/kzx/luban_spring_v1/LubanSpringV1Application.java
@@ -0,0 +1,13 @@
+package com.kzx.luban_spring_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LubanSpringV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LubanSpringV1Application.class, args);
+	}
+
+}
